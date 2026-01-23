150a294f47d08ec88cb4b657005e43fbe4b509a9
Isaac <barkerisaacd@gmail.com>
2026-01-22 19:22:57 -0500

    add measured LL3G fov to camera constants

--- src/main/java/frc/robot/utils/camera/CameraType.java
+++ src/main/java/frc/robot/utils/camera/CameraType.java
@@ -2,9 +2,7 @@ package frc.robot.utils.camera;
 
 public enum CameraType {
     LIMELIGHT_2(1280, 960, 2, 74.36, 13, 0.2, 0.0005, 35, 5, 0.55, 1.4175, 0.35),
-    LIMELIGHT_3G(
-            1280, 960, 1, 100, 8, 0.2, 0.0005, 35, 5, 0.55, 1.11574,
-            0.35), // TODO: what is the actual fov of new lens we replaced
+    LIMELIGHT_3G(1280, 960, 1, 75.8, 8, 0.2, 0.0005, 35, 5, 0.55, 1.11574, 0.35),
     SVPRO_GLOBAL_SHUTTER(1920, 1200, 1, 83, 23, 0.2, 0.0005, 35, 5, 0.55, 1.3, 0.35);
 
     final int width;
