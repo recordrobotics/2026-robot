cd361628f5279fafb33a080a01b4235c0eefacbf
Compdog Inc. <compdog.inc@gmail.com>
2026-01-28 16:00:37 -0500

    Fix warning

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,5 +7,7 @@ public final class RobotMap {
         public static final int ARM_LEADER_ID = 15; // TODO
         public static final int ARM_FOLLOWER_ID = 16; // TODO
         public static final int WHEEL_ID = 17; // TODO
+
+        private Intake() {}
     }
 }
