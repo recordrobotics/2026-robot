896ea2ab387ced03cbbd3c29651cc1787dceefa9
Cruiserplays <231336941+Cruiserplays@users.noreply.github.com>
2026-06-11 20:19:00 -0400

    Add inherited methods to JoystickKidControls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -359,7 +359,7 @@ public final class RobotContainer {
                 .onFalse(Commands.runOnce(
                         () -> {
                             returnToOverviewTabIfIntakeStarting();
-                            intake.setState(Intake.IntakeState.DOWN);
+                            intake.setState(Intake.IntakeState.OUT);
                         },
                         intake,
                         climber));
